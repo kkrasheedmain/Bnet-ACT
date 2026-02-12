09f98505c94c42b759ad698a04cb98a75ee334bd
kkrasheedmain <kkrasheedmain@gmail.com>
2026-02-12 16:40:29 +0530

    Main frame creation in mainfile_loop and dscm_frame ok

--- dscm_frame.py
+++ dscm_frame.py
@@ -1,36 +1,4 @@
-
-
-
 import customtkinter as ctk
-
-def create_side_frame(parent):
-    side_frame = ctk.CTkFrame(parent, width=150, border_width=1,
-                              border_color='red', fg_color='coral1')
-
-    side_frame_colour = side_frame.cget('fg_color')
-
-    buttons = [
-        ("DSCM BILL", 30),
-        ("CBP BILL", 130),
-        ("CTOP-UP BILL", 230),
-        ("OTHER-COLLECTION", 330),
-        ("EXPENSE", 430),
-        ("STOCK PURCHASE", 530),
-    ]
-
-    for text, y in buttons:
-        btn = ctk.CTkButton(
-            side_frame,
-            text=text,
-            text_color='white',
-            font=('Bold', 15),
-            width=60,
-            hover=False,
-            fg_color=side_frame_colour
-        )
-        btn.place(x=5 if y >= 330 else 10, y=y)
-
-    return side_frame
 
 ###########################
 def create_dscm_frame(parent):
--- mainfile_loop.py
+++ mainfile_loop.py
@@ -1,5 +1,40 @@
 import customtkinter as ctk
-from support_functions import create_side_frame,create_dscm_frame
+from dscm_frame import create_dscm_frame
+
+
+def create_side_frame(parent):
+    side_frame = ctk.CTkFrame(parent, width=150, border_width=1,
+                              border_color='red', fg_color='coral1')
+
+    side_frame_colour = side_frame.cget('fg_color')
+
+    buttons = [
+        ("DSCM BILL", 30),
+        ("CBP BILL", 130),
+        ("CTOP-UP BILL", 230),
+        ("OTHER-COLLECTION", 330),
+        ("EXPENSE", 430),
+        ("STOCK PURCHASE", 530),
+    ]
+
+    for text, y in buttons:
+        btn = ctk.CTkButton(
+            side_frame,
+            text=text,
+            text_color='white',
+            font=('Bold', 15),
+            width=60,
+            hover=False,
+            fg_color=side_frame_colour
+        )
+        btn.place(x=5 if y >= 330 else 10, y=y)
+
+    return side_frame
+
+
+
+
+
 
 if __name__ == '__main__':
 
@@ -15,8 +50,8 @@ if __name__ == '__main__':
     side_frame.pack(side='left', fill='y', padx=5, pady=5)
 
     #main_frame
-    main_frame= create_dscm_frame(window)
-    main_frame.pack(side='left', fill='both', expand=True, pady=5, padx=5)
+    dscm_frame= create_dscm_frame(window)
+    dscm_frame.pack(side='left', fill='both', expand=True, pady=5, padx=5)
 
 
 
